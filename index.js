12c277a262cd04d3b875547921bf4e564e08c1b8
phil <uniphil@gmail.com>
2015-11-21 16:05:25 -0500

    split lib/app scripts

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 'use strict';
 
 
-const isHandler = attr => attr.startsWith('on');
-
-
-const d = name => (attrs, children) => {
-  const el = document.createElement(name);
-  Object.keys(attrs)
-    .filter(attr => !!attrs[attr])
-    .forEach(attr =>
-      attr.startsWith('on') ?
-        el.addEventListener(attr.slice(2).toLowerCase(), attrs[attr]) :
-        el.setAttribute(attr, attrs[attr]));
-  children.forEach(child =>
-    el.appendChild(child));
-  return el;
-};
-
-const t = s => document.createTextNode(s);
-const div = d('div');
-const button = d('button');
-
 const ADD_COUNTER = Symbol('ADD_COUNTER');
 const REMOVE_COUNTER = Symbol('REMOVE_COUNTER');
 const INCREMENT = Symbol('INCREMENT');
@@ -37,13 +17,20 @@ const Counter = connect => state =>
   ]);
 
 
-const createReducer = (initialState, handlers) => (prevState, action, payload) => {
-  if (typeof handlers[action] !== 'undefined') {
-    return handlers[action](prevState, payload);
-  } else {
-    return (typeof prevState !== 'undefined') ? prevState : initialState;
-  }
-};
+const Counters = connect => state =>
+  div({}, [
+    div({}, [
+      button({
+        onClick: connect(REMOVE_COUNTER),
+        disabled: state.length < 1,
+      }, [t('remove')]),
+      button({
+        onClick: connect(ADD_COUNTER)
+      }, [t('add')]),
+    ]),
+    div({}, state.map((count, i) =>
+      Counter(action => () => connect(action)(i))(state[i]))),
+  ]);
 
 
 function setAt(arr, i, v) {
@@ -66,46 +53,6 @@ const countersReducer = createReducer([], ({
   [DECREMENT]: (state, i) => setAt(state, i, state[i] - 1),
   [INCREMENT]: (state, i) => setAt(state, i, state[i] + 1),
 }));
-
-
-function replaceChildren(el, newNode) {
-  while (el.lastChild) {
-    el.lastChild.remove();
-  }
-  el.appendChild(newNode);
-}
-
-
-const Counters = connect => state =>
-  div({}, [
-    div({}, [
-      button({
-        onClick: connect(REMOVE_COUNTER),
-        disabled: state.length < 1,
-      }, [t('remove')]),
-      button({
-        onClick: connect(ADD_COUNTER)
-      }, [t('add')]),
-    ]),
-    div({}, state.map((count, i) =>
-      Counter(action => () => connect(action)(i))(state[i]))),
-  ]);
-
-
-function render(reducer, Component, el) {
-  var state, oldTree;
-
-  const boundComponent = Component(
-    action => payload => dispatch(action, payload)
-  );
-
-  const dispatch = (action, payload) => {
-    state = reducer(state, action, payload);
-    replaceChildren(el, boundComponent(state));
-  };
-
-  dispatch();
-}
 
 
 render(countersReducer, Counters, document.getElementById('app'));
---
+++ recoil.js
@@ -0,0 +1,66 @@
+'use strict';
+
+
+(function(global) {
+  const isHandler = attr => attr.startsWith('on');
+
+  const d = name => (attrs, children) => {
+    const el = document.createElement(name);
+    Object.keys(attrs)
+      .filter(attr => !!attrs[attr])
+      .forEach(attr =>
+        attr.startsWith('on') ?
+          el.addEventListener(attr.slice(2).toLowerCase(), attrs[attr]) :
+          el.setAttribute(attr, attrs[attr]));
+    children.forEach(child =>
+      el.appendChild(child));
+    return el;
+  };
+
+  const t = s => document.createTextNode(s);
+  const div = d('div');
+  const button = d('button');
+
+
+  const createReducer = (initialState, handlers) => (prevState, action, payload) => {
+    if (typeof handlers[action] !== 'undefined') {
+      return handlers[action](prevState, payload);
+    } else {
+      return (typeof prevState !== 'undefined') ? prevState : initialState;
+    }
+  };
+
+
+  function replaceChildren(el, newNode) {
+    while (el.lastChild) {
+      el.lastChild.remove();
+    }
+    el.appendChild(newNode);
+  }
+
+
+  function render(reducer, Component, el) {
+    var state, oldTree;
+
+    const boundComponent = Component(
+      action => payload => dispatch(action, payload)
+    );
+
+    const dispatch = (action, payload) => {
+      state = reducer(state, action, payload);
+      replaceChildren(el, boundComponent(state));
+    };
+
+    dispatch();
+  }
+
+
+  Object.assign(global, {
+    t,
+    div,
+    button,
+    createReducer,
+    render,
+  });
+
+})(window);
