c2ee225467824f5ae3a05222c8fb3ce08cb9ca52
phil <uniphil@gmail.com>
2015-11-22 12:06:22 -0500

    n counters -- slightly more generalized

--- index.js
+++ index.js
@@ -1,46 +1,59 @@
 'use strict';
 
 
-const ADD_COUNTER = Symbol('ADD_COUNTER');
-const REMOVE_COUNTER = Symbol('REMOVE_COUNTER');
-const WRAP_COUNTER = Symbol('WRAP_COUNTER');
-const INCREMENT = Symbol('INCREMENT');
-const DECREMENT = Symbol('DECREMENT');
+/// Counter: actions, view, reducer
 
+const counterActions = {
+  INCREMENT: Symbol('INCREMENT'),
+  DECREMENT: Symbol('DECREMENT'),
+};
 
 const Counter = connect => state =>
   div({}, [
-    button({ onClick: connect(DECREMENT) }, [
+    button({ onClick: connect(counterActions.DECREMENT) }, [
       t('-')]),
     t(state),
-    button({ onClick: connect(INCREMENT) }, [
+    button({ onClick: connect(counterActions.INCREMENT) }, [
       t('+')]),
   ]);
 
-
 const counterReducer = createReducer(0, {
-  [INCREMENT]: state => state + 1,
-  [DECREMENT]: state => state - 1,
+  [counterActions.INCREMENT]: state => state + 1,
+  [counterActions.DECREMENT]: state => state - 1,
 });
 
 
-const Counters = connect => state =>
+/// List: actions, view, reducer
+
+const listActions = {
+  ADD: Symbol('ADD'),
+  REMOVE: Symbol('REMOVE'),
+  CHILD_ACTION: Symbol('CHILD_ACTION'),
+};
+
+const List = connect => state =>
   div({}, [
     div({}, [
       button({
-        onClick: connect(REMOVE_COUNTER),
+        onClick: connect(listActions.REMOVE),
         disabled: state.length < 1,
       }, [t('remove')]),
       button({
-        onClick: connect(ADD_COUNTER)
+        onClick: connect(listActions.ADD)
       }, [t('add')]),
     ]),
     div({}, state.map((count, i) =>
-      Counter(action => payload => connect(WRAP_COUNTER)({
-        action,
-        payload,
-        i,
-      }))(state[i]))),
+      Counter(action => {
+        if (Object.keys(counterActions).some(k => counterActions[k] === action)) {
+          return payload => connect(listActions.CHILD_ACTION)({
+            action,
+            payload,
+            i
+          });
+        } else {
+          return connect(action);
+        }
+      })(state[i]))),
   ]);
 
 
@@ -57,13 +70,13 @@ function pop(arr) {
   return copy;
 }
 
-const countersReducer = createReducer([], {
-  [ADD_COUNTER]: state => state.concat([counterReducer()]),
-  [REMOVE_COUNTER]: state => pop(state),
-  [WRAP_COUNTER]: (state, payload) =>
+const listReducer = createReducer([], {
+  [listActions.ADD]: state => state.concat([counterReducer()]),
+  [listActions.REMOVE]: state => pop(state),
+  [listActions.CHILD_ACTION]: (state, payload) =>
     setAt(state, payload.i,
       counterReducer(state[payload.i], payload.action, payload.payload)),
 });
 
 
-render(countersReducer, Counters, document.getElementById('app'));
+render(listReducer, List, document.getElementById('app'));
